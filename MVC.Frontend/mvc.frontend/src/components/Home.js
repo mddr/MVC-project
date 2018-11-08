7540d8bcc6ab011b6300b9b401d8825af6c57f4a
Damian <damianjazwinski@github.com>
2018-11-08 21:27:31 +0100

    Side nav menu added but not working properly.

--- MVC.Frontend/mvc.frontend/src/components/Home.js
+++ MVC.Frontend/mvc.frontend/src/components/Home.js
@@ -2,26 +2,40 @@ import "./Home.css";
 
 import React, { Component } from "react";
 import ProductSlider from "./ProductSlider";
-import { SideNav } from "react-sidenav";
+import { SideNav, Nav } from "react-sidenav";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isUserLogged: false
+      isUserLogged: false,
+      categories: [
+        { id: 1, name: "Odzież męskia", superiorCatId: null },
+        { id: 2, name: "Spodnie", superiorCatId: 1 },
+        { id: 3, name: "Buty", superiorCatId: 1 },
+        { id: 4, name: "Buty sportowe", superiorCatId: 3 },
+        { id: 5, name: "Pantofle", superiorCatId: 3 },
+        { id: 6, name: "Odzież damska", superiorCatId: null }
+      ]
     };
   }
+  displayCategories = element => {
+    console.log("" + element);
+    let smth = !element.superiorCatId ? <Nav>{element.name} </Nav> : "";
+    return smth;
+  };
 
   render() {
     return (
       <div className="home">
         <div className="ad">
           <div className="ad_img" />
-          {/* <img src="img/hangers_long.png" alt="hangers_long" /> */}
         </div>
         <div className="categories">
-          <SideNav />
+          <SideNav className="sidemenu">
+            {this.state.categories.map(this.displayCategories)}
+          </SideNav>
         </div>
         <div className="sale">
           Promocje
