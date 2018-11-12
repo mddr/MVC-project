948d260845123eced624697b78d8098716bb6528
Damian <damianjazwinski@github.com>
2018-11-12 23:09:06 +0100

    Poprawione wyświetlanie ikony koszyka i slidery

--- MVC.Frontend/mvc.frontend/src/components/Cart.jsx
+++ MVC.Frontend/mvc.frontend/src/components/Cart.jsx
@@ -8,74 +8,93 @@ import CartService from "../services/CartService";
 import ProductService from "../services/ProductService";
 
 class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalPrice: 0
+    };
+    this.getTotalPrice = this.getTotalPrice.bind(this);
+    this.renderItems = this.renderItems.bind(this);
+    this.CartService = new CartService();
+    this.ProductService = new ProductService();
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            totalPrice: 0,
-        };
-        this.getTotalPrice = this.getTotalPrice.bind(this);
-        this.renderItems = this.renderItems.bind(this);
-        this.CartService = new CartService();
-        this.ProductService = new ProductService();
+  componentDidUpdate(prevProps) {
+    if (this.props.cartItems !== prevProps.cartItems) {
+      this.getTotalPrice();
     }
+  }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.cartItems !== prevProps.cartItems) {
-            this.getTotalPrice();
-        } 
-    }
-
-    getTotalPrice() {
-        if (this.props.cartItems.length < 1) return 0;
-        for (let i = 0; i < this.props.cartItems.length; i++) {
-            this.ProductService.getProduct(this.props.cartItems[i].productId).then(res => res.json()).then(res => {
-                this.setState({
-                    totalPrice: this.state.totalPrice + Math.round(this.props.cartItems[i].productAmount * res.pricePln * 100) / 100
-                });
-            });
-        }
-    }
-
-    renderItems() {
-        if (this.props.cartItems.length < 1) return;
-        let items = []
-        this.props.cartItems.map(item => {
-            this.ProductService.getProduct(item.productId).then(res => res.json()).then(res => {
-                items.push(<div className="item">
-                    <img src={"data:image/jpeg;base64," + res.imageBase64} alt={res.name} height="64" width="64" />
-                    <span className="namespan">
-                        {item.productAmount} x {res.name}
-                    </span>
-                    <span>
-                        {Math.round(res.pricePln * item.productAmount * 100) / 100}
-                        zł
-                      </span>
-                    <span />
-                </div>)
-                })
+  getTotalPrice() {
+    if (this.props.cartItems.length < 1) return 0;
+    for (let i = 0; i < this.props.cartItems.length; i++) {
+      this.ProductService.getProduct(this.props.cartItems[i].productId)
+        .then(res => res.json())
+        .then(res => {
+          this.setState({
+            totalPrice:
+              this.state.totalPrice +
+              Math.round(
+                this.props.cartItems[i].productAmount * res.pricePln * 100
+              ) /
+                100
+          });
         });
-        
-        return items;
     }
+  }
 
-    render() {
-        const { cartItems } = this.props;
+  renderItems() {
+    if (this.props.cartItems.length < 1) return;
+    let items = [];
+    this.props.cartItems.map(item => {
+      this.ProductService.getProduct(item.productId)
+        .then(res => res.json())
+        .then(res => {
+          items.push(
+            <div className="item">
+              <img
+                src={"data:image/jpeg;base64," + res.imageBase64}
+                alt={res.name}
+                height="64"
+                width="64"
+              />
+              <span className="namespan">
+                {item.productAmount} x {res.name}
+              </span>
+              <span>
+                {Math.round(res.pricePln * item.productAmount * 100) / 100}
+                zł
+              </span>
+            </div>
+          );
+        });
+    });
+
+    return items;
+  }
+
+  render() {
+    const { cartItems } = this.props;
     const popoverClickRootClose =
       cartItems.length > 0 ? (
         <Popover
           id="popover-trigger-click-root-close"
           title="Koszyk"
-          arrowOffsetTop="80"
+          positionTop="100"
+          positionLeft="100"
+          arrowOffsetTop="100"
+          arrowOffsetLeft="100"
         >
-        {this.renderItems()}
+          {this.renderItems()}
           <hr />
           <div className="cartsummary">
-            <Link to="/order"><Button bsStyle="buyButton" bsSize="medium">
-              <Glyphicon glyph="ok" /> Złóż zamówienie
-            </Button></Link>
-                    <div className="sumatext">Suma: </div>
-                    <div>{this.state.totalPrice}zł</div>
+            <Link to="/order">
+              <Button bsStyle="buyButton" bsSize="large">
+                <Glyphicon glyph="ok" /> Złóż zamówienie
+              </Button>
+            </Link>
+            <div className="sumatext">Suma: </div>
+            <div>{this.state.totalPrice}zł</div>
           </div>
         </Popover>
       ) : (
--- MVC.Frontend/mvc.frontend/src/components/OrderPage.jsx
+++ MVC.Frontend/mvc.frontend/src/components/OrderPage.jsx
@@ -7,77 +7,96 @@ import CartService from "../services/CartService";
 import ProductService from "../services/ProductService";
 
 class OrderPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalPrice: 0
+    };
+    this.getTotalPrice = this.getTotalPrice.bind(this);
+    this.placeOrder = this.placeOrder.bind(this);
+    this.renderItems = this.renderItems.bind(this);
+    this.CartService = new CartService();
+    this.ProductService = new ProductService();
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            totalPrice: 0,
-        };
-        this.getTotalPrice = this.getTotalPrice.bind(this);
-        this.placeOrder = this.placeOrder.bind(this);
-        this.renderItems = this.renderItems.bind(this);
-        this.CartService = new CartService();
-        this.ProductService = new ProductService();
+  componentDidUpdate(prevProps) {
+    if (this.props.cartItems !== prevProps.cartItems) {
+      this.getTotalPrice();
     }
+  }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.cartItems !== prevProps.cartItems) {
-            this.getTotalPrice();
-        }
-    }
-
-    getTotalPrice() {
-        if (this.props.cartItems.length < 1) return 0;
-        for (let i = 0; i < this.props.cartItems.length; i++) {
-            this.ProductService.getProduct(this.props.cartItems[i].productId).then(res => res.json()).then(res => {
-                this.setState({
-                    totalPrice: this.state.totalPrice + Math.round(this.props.cartItems[i].productAmount * res.pricePln * 100) / 100
-                });
-            });
-        }
-    }
-        
-    placeOrder() {
-        let body = "";
-        const obj = {
-            userId: this.state.id,
-            cartId: this.state.name,
-            totalPrice: this.state.pricePln,
-        };
-
-        body = JSON.stringify(obj);
-
-        this.props.Auth.fetch(`${this.props.Auth.domain}/order/add`, {
-            method: 'post',
-            body
+  getTotalPrice() {
+    if (this.props.cartItems.length < 1) return 0;
+    for (let i = 0; i < this.props.cartItems.length; i++) {
+      this.ProductService.getProduct(this.props.cartItems[i].productId)
+        .then(res => res.json())
+        .then(res => {
+          this.setState({
+            totalPrice:
+              this.state.totalPrice +
+              Math.round(
+                this.props.cartItems[i].productAmount * res.pricePln * 100
+              ) /
+                100
+          });
         });
     }
+  }
 
-    renderItems() {
-        if (this.props.cartItems.length < 1) return;
-        let items = []
-        this.props.cartItems.map(item => {
-            this.ProductService.getProduct(item.productId).then(res => res.json()).then(res => {
-                items.push(<div className="item">
-                    <img src={"data:image/jpeg;base64," + res.imageBase64} alt={res.name} height="64" width="64" />
-                    <span className="namespan">
-                        {item.productAmount} x {res.name}
-                    </span>
-                    <span>
-                        {Math.round(res.pricePln * item.productAmount * 100) / 100}
-                        zł
-                      </span>
-                    <span />
-                </div>);
-                
-            })
+  placeOrder() {
+    let body = "";
+    const obj = {
+      userId: this.state.id,
+      cartId: this.state.name,
+      totalPrice: this.state.pricePln
+    };
+
+    body = JSON.stringify(obj);
+
+    this.props.Auth.fetch(`${this.props.Auth.domain}/order/add`, {
+      method: "post",
+      body
+    });
+  }
+
+  renderItems() {
+    if (this.props.cartItems.length < 1) return;
+    let items = [];
+    this.props.cartItems.map(item => {
+      this.ProductService.getProduct(item.productId)
+        .then(res => res.json())
+        .then(res => {
+          items.push(
+            <div className="item">
+              <img
+                src={"data:image/jpeg;base64," + res.imageBase64}
+                alt={res.name}
+                height="64"
+                width="64"
+              />
+              <span className="namespan">
+                {item.productAmount} x {res.name}
+              </span>
+              <span>
+                {Math.round(res.pricePln * item.productAmount * 100) / 100}
+                zł
+              </span>
+              <span />
+            </div>
+          );
         });
-        
-        return items;
-    }
+    });
 
-    render() {
-        const items = this.props.cartItems.length > 0 ? this.renderItems() : (<div>dupa</div>);
+    return items;
+  }
+
+  render() {
+    const items =
+      this.props.cartItems.length > 0 ? (
+        this.renderItems()
+      ) : (
+        <div>Zamówienie jest puste</div>
+      );
     return (
       <div className="orderpage">
         <div className="banner">
@@ -85,12 +104,12 @@ class OrderPage extends Component {
         </div>
         <div className="summary">
           <p>Zamówienie:</p>
-            <hr style={{ width: "95%" }} />
-                {items}
+          <hr style={{ width: "95%" }} />
+          {items}
           <hr style={{ width: "95%" }} />
           <div className="ordersum">
-                    <div className="ordersumtext">Suma: </div>
-                    <div>{this.state.totalPrice}zł</div>
+            <div className="ordersumtext">Suma: </div>
+            <div>{this.state.totalPrice}zł</div>
           </div>
         </div>
         <div className="shippinginfo">
