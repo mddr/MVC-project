9cd7d1e4a53c034a5c23b14dd63484aab7b993d4
Ajoni <adam584@onet.pl>
2018-11-12 18:09:39 +0100

    forgot to pass props

--- MVC.Frontend/mvc.frontend/src/Routes.js
+++ MVC.Frontend/mvc.frontend/src/Routes.js
@@ -16,8 +16,8 @@ class Routes extends Component {
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/admin" component={AdminPanel} />
-                <Route exact path="/product/:id" render={() => <OrderPage cartItems={this.props.cartItems} />} />
-                <Route exact path="/order" render={() => <OrderPage cartItems={this.props.cartItems} />}  />
+                <Route exact path="/product/:id" render={(props) => <ProductPage {...props} cartItems={this.props.cartItems} />} />
+                <Route exact path="/order" render={(props) => <OrderPage {...props} cartItems={this.props.cartItems} />}  />
             </Switch>
         );
     }
