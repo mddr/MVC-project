848e3cb23c929fb13e3c6e559ea0bc830ce192f2
Ajoni <adam584@onet.pl>
2018-11-26 19:43:09 +0100

    fixed product form and auth domain

--- MVC.Frontend/mvc.frontend/src/components/ProductForm.js
+++ MVC.Frontend/mvc.frontend/src/components/ProductForm.js
@@ -27,7 +27,7 @@ export default class ProductForm extends React.Component{
 	validateForm() {
 		if (!(this.state.name.length > 0 && this.state.pricePln > 0 && this.state.categoryId > 0 
             && this.state.amountAvailable > -1 && this.state.discount >= 0 && this.state.discount <= 100
-            && this.state.discount.length > 0 && this.state.amountAvailable.length > 0))
+            && this.state.discount !=="" && this.state.amountAvailable !==""))
 		  return false;
 		return true;
 	}
